49438946c986fb5d4c285151b57e924997c6cfc3
Arif <arifkavas@gmail.com>
2017-01-24 14:12:22 +0300

    initial commit

---
+++ Uart.h
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////
+//Project Name	: MIP											/
+//File Name		: Uart.h										/
+//Author		: Arif Kursad KAVAS								/
+//Revision		: 0.1 											/
+//Date			: 17/04/08										/
+//																/
+//																/
+//																/
+//																/
+/////////////////////////////////////////////////////////////////
+
+#ifndef _INC_UART
+#define _INC_UART
+
+int InitUart(void);
+int	TransmitMessage(int *Msg, int Length);
+
+
+#endif
